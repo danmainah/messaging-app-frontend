91ae3478bf5f24bc1e18d91f147cdbd230dfab4f
Danmainah <danielmainawaweru@gmail.com>
2024-03-13 17:18:20 +0300

    create a chat app

---
+++ src/components/Sidebar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Sidebar.css";
+import { Avatar } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchOutlined from "@mui/icons-material/SearchOutlined";
+import SidebarChat from './SidebarChat'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src="https://pbs.twimg.com/profile_images/1020939891457241088/fcbu814K_400x400.jpg" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
+};
+export default Sidebar;
